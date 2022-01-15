5a4c1707647365416a8eaf8e47da4aea30fab25d
Paulo Cesar Ramos <paulo.1209@terra.com.br>
2022-01-15 15:52:48 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,28 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect } from "react";
+import { useState } from "react";
+import { MoviePage } from "types/movie";
+import { BASE_URL } from "utils/request";
 
 function Listing(){
+
+
+const [pgeNumber, setPageNumber] = useState(0);
+
+useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    .then(response =>{
+        const data = response.data as MoviePage;
+        console.log(data);
+        setPageNumber(data.number)
+    })
+}, [])
+
+  
+
+
     return (
         <>
          <Pagination/>
