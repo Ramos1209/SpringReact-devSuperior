35a570a6106794e0754285e5e67152de1f5ca48c
Paulo Cesar Ramos <paulo.1209@terra.com.br>
2022-01-15 17:10:27 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,8 +16,8 @@ function MovieCard({movie} : Props) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
-                <Link to="form/1">
+                <MovieScore score={movie.score} count={movie.count} />
+                <Link to={`/form/${movie.id}`}>
                 <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
               
--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,18 @@
 import MovieStars from "components/MovieStars";
 import './stile.css'
 
+type Props ={
+    score: number,
+    count: number,
+}
 
-function MovieScore() {
+function MovieScore({score, count }:Props) {
 
-    const score = 3.0
-    const count = 4
+   
     return (
         <div className="dsmovie-score-container">
             <p className="dsmovie-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score} />
             <p className="dsmovie-score-count">{count} avaliações</p>
         </div>
 
