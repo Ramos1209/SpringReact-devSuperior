395acc60e2ae532cb0f72f6d95c160d21e41afdd
Paulo Cesar Ramos <paulo.1209@terra.com.br>
2022-01-16 01:02:44 -0300

    correção

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -7,10 +7,10 @@ function NavBar(){
         <nav className="container">
             <div className="dsmovie-nav-content">
                 <h1>DSMovie</h1>
-                <a href="https://github.com/devsuperior" target="_blank" rel="noreferrer">
+                <a href="https://github.com/Ramos1209" target="_blank" rel="noreferrer">
                     <div className="dsmovie-contact-container">
                         <GithubIcon />
-                        <p className="dsmovie-contact-link">/devsuperior</p>
+                        <p className="dsmovie-contact-link">/R@mos1209</p>
                     </div>
                 </a>
             </div>
